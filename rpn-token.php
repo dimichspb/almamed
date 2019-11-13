c6ec33c7474d383a42af406d0cee2df9ccb025b7
Dmitry Tarantin <dimichspb@gmail.com>
2019-11-13 11:18:34 +0100

    Html output

--- rpn-token.php
+++ rpn-token.php
@@ -11,11 +11,30 @@ $params = [
     'grant_type' => 'password',
 ];
 
-$response = $client->post('http://5.104.236.197:22999/oauth/token', [
-    'form_params' => $params,
-    'proxy' => $proxy,
-    'debug' => true,
-]);
-
-echo $response->getBody();
+ob_start();
+try {
+    $response = $client->post('http://5.104.236.197:22999/oauth/token', [
+        'form_params' => $params,
+        'proxy' => $proxy,
+        'debug' => true,
+    ]);
+    $body = $response->getBody();
+} catch (\Exception $exception) {
+    $body = $exception->getMessage();
+}
+$verbose = ob_get_contents();
+ob_end_clean();
+?>
+<html>
+<body>
+<div style="width: 100%;">
+    <label for="verbose">Verbose:</label>
+    <textarea id="verbose" readonly><?= $verbose ?></textarea>
+</div>
+<div style="width: 100%;">
+    <label for="body">Body:</label>
+    <textarea id="body"><?= $body ?></textarea>
+</div>
+</body>
+</html>
 
