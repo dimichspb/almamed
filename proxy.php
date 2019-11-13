d2b6e4e8b5fc89534051b5085e1ba9df4b7a8b6f
Tarantin <Dmitry.Tarantin@telekom.de>
2019-11-13 11:00:56 +0100

    Init commit

---
+++ proxy.php
@@ -0,0 +1,10 @@
+<?php
+
+$settings = [
+    'host' => '94.247.131.22',
+    'port' => 3128,
+    'username' => 'almamed',
+    'password' => 'e09rg7be90g87sdf',
+];
+
+return $settings['username'] . ':' . $settings['password'] . '@' . $settings['host'] . ':' . $settings['port'];
---
+++ rpn-token.php
@@ -0,0 +1,20 @@
+<?php
+
+require ("vendor/autoload.php");
+$proxy = require ("proxy.php");
+
+$client = new GuzzleHttp\Client();
+
+$params = [
+    'username' => '850829324948',
+    'password' => '123456',
+    'grant_type' => 'password',
+];
+
+$response = $client->post('http://5.104.236.197:777/oauth/token', [
+    'form_params' => $params,
+    'proxy' => $proxy,
+]);
+
+echo $response->getBody();
+
